a23756c34cd96ab1ae095d95bd778b88c5ce9d97
JPetrozzi <juanpablopetrozzi@gmail.com>
2017-06-27 22:32:01 -0300

    Add Cors Configuration

---
+++ list-api/src/main/java/dev/jpp/listapi/config/RestConfig.java
@@ -0,0 +1,29 @@
+package dev.jpp.listapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class RestConfig {
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource src = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("DELETE");
+		src.registerCorsConfiguration("/**", config);
+		
+		return new CorsFilter(src);
+	}
+	
+}
